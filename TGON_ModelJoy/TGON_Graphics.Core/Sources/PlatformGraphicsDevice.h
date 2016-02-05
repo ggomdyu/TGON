f4495f667b1a06f898cf562b85fbb9a61adc9a51
차준호 <ggomdyu@gmail.com>
2016-02-05 11:21:57 +0900

    deleted

--- TGON_ModelJoy/TGON_Graphics.Core/Sources/PlatformGraphicsDevice.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#ifdef PLATFORM_WINDOWS_OS
-	#include "..\..\TGON_DirectX\Sources\D3d9Device.h"
-#elif PLATFORM_ANDROID_OS
-
-#endif
--- TGON_ModelJoy/TGON_Platform.Core/Sources/PlatformWindow.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#ifdef PLATFORM_WINDOWS_OS
-	#include "..\..\TGON_Windows.Core\Sources\WindowsWindow.h"
-#else
-	#error	 "You're platform is not supported."
-#endif
