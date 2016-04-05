c1f58807d4433873a795d5c12c9df22f297beeec
차준호 <ggomdyu@gmail.com>
2016-04-06 00:48:13 +0900

    Excluded

---
+++ Source/TGON/Component/IComponent.h
@@ -0,0 +1,56 @@
+/*
+* 작성자 : 차준호
+* 작성일 : 2015-04-26
+* 최종 수정 :
+* 최종 수정일 :
+*/
+
+
+#pragma once
+#include <stdint.h>
+#include "../Object/TGameObject.h"
+
+
+namespace tgon
+{
+
+
+class TGameObject;
+class IComponent
+{
+public:
+	IComponent( ) : m_owner( nullptr ) {};
+	virtual ~IComponent( ) = 0 {};
+
+	virtual void Update( float elapsedTime ) = 0;
+
+public:
+	void SetOwner( TGameObject* const owner );
+	TGameObject* GetOwner( ) const;
+
+	template <typename T>
+	static uint32_t GetIdentifier( );
+
+private:
+	TGameObject* m_owner;
+};
+
+
+}
+
+void tgon::IComponent::SetOwner( TGameObject* const owner )
+{
+	m_owner = owner;
+}
+
+inline TGameObject* tgon::IComponent::GetOwner( )
+{
+	return m_owner;
+}
+
+template<typename T>
+inline uint32_t tgon::IComponent::GetIdentifier( )
+{
+	static const auto keyCode( typeid(T).hash_code( ));
+	return keyCode;
+}
--- TGON_ModelJoy/lib/freetype/ReadMe.txt
+++
@@ -1,7 +0,0 @@
-////////////////////////////////////////
-
-== Description ==
-
-
-¡Ø Windows
-All of libraries compiled by VS2013. So DO NOT link them under VS2013 version.
