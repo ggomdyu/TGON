fee4d2e5b9512b4aaa4f1d467fd8817c9e65d425
ggomdyu <ggomdyu@GGOMDYU-PC>
2016-04-14 01:41:46 +0900

    GetCursorPos member function deleted

--- Source/TGON/Application/Windows/WindowsApplication.h
+++ Source/TGON/Application/Windows/WindowsApplication.h
@@ -1,17 +1,15 @@
 /*
-* 작성자 : 차준호
-* 작성일 : 2016-03-20
-* 최종 수정 :
-* 최종 수정일 :
+* Author : Junho-Cha
+* Date : 2016-03-20
+* Latest author :
+* Latest date :
 * Description : Abstracted Windows API & Util
 */
 
 #pragma once
 #include "../Layered/LayeredApplication.h"
 
-#define WIN32_LEAN_AND_MEAN
-	#include <Windows.h>
-#undef WIN32_LEAN_AND_MEAN
+#include <minwindef.h>
 
 
 namespace tgon
@@ -27,6 +25,7 @@ class WindowsApplication : public LayeredApplication
 protected:
 	WindowsApplication( ) :
 		m_instanceHandle( GetModuleHandle( NULL )) {}
+	
 	virtual ~WindowsApplication( ) {}
 
 public:
@@ -34,12 +33,9 @@ public:
 	
 	HINSTANCE GetInstanceHandle( ) const;
 
-	virtual void GetScreenSize( int32_t* const width,
-							    int32_t* const height ) override;
-	
-	virtual void GetCursorPosition( int32_t* const x,
-								    int32_t* const y ) override;
-	
+	virtual void GetScreenSize( _Out_ int32_t* const width,
+								_Out_ int32_t* const height ) override;
+
 	virtual struct TSystemBatteryInfo GetPowerInfo( ) override;
 
 private:
@@ -47,7 +43,19 @@ private:
 };
 
 
+inline HINSTANCE WindowsApplication::GetInstanceHandle( ) const
+{
+	return m_instanceHandle;
 }
 
 
-#include "WindowsApplication.hpp"
+inline void WindowsApplication::GetScreenSize(
+	int32_t* const width,
+	int32_t* const height )
+{
+	*width = GetSystemMetrics( SM_CXSCREEN );
+	*height = GetSystemMetrics( SM_CYSCREEN );
+}
+
+
+}
