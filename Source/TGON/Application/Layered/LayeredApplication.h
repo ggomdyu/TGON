4597e722bd6b69251b5abcdebdc15cf73bfd2053
ggomdyu <ggomdyu@GGOMDYU-PC>
2016-04-24 21:06:11 +0900

    Change class layout. And inherited from Noncopyable

--- Source/TGON/Application/Layered/LayeredApplication.h
+++ Source/TGON/Application/Layered/LayeredApplication.h
@@ -1,34 +1,35 @@
 /*
 * Author : Junho-Cha
-* Date : 2016-03-20
+* Date : 03/20/2016
 * Latest author :
 * Latest date :
-* Description : Abstracted platform API & Util
+* Description : Abstracted platform API
 */
 
 #pragma once
-#include "../../Window/TWindow.h"
-#include <map>
 #include <stdint.h>
+#include "../../Core/TNoncopyable.h"
 
 
 namespace tgon
 {
 
 
-class LayeredApplication
+class LayeredApplication : Noncopyable
 {
+private:
+	virtual bool DispatchEvent( 
+		enum struct WindowEvent* const ) = 0;
+
+	virtual void GetScreenSize(
+		int32_t* width,
+		int32_t* height ) = 0;
+
+	virtual struct TSystemBatteryInfo GetPowerInfo( ) = 0;
+
 protected:
 	LayeredApplication( ) {};
 	virtual ~LayeredApplication( ) = 0 {};
-
-private:
-	virtual void DispatchEvent( _Out_ enum struct WindowEvent* const ) = 0;
-
-	virtual void GetScreenSize( int32_t* const width,
-								int32_t* const height ) = 0;
-
-	virtual struct TSystemBatteryInfo GetPowerInfo( ) = 0;
 };
 
 
