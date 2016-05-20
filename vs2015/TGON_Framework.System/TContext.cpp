c28ee3766a555c61813261f0a5ab90de4c4fe3e4
ggomdyu <ggomdyu@GGOMDYU-PC>
2016-05-20 11:45:51 +0900

    Add

---
+++ vs2015/TGON_Framework.System/TContext.cpp
@@ -0,0 +1,2 @@
+#include "PrecompiledHeader.h"
+#include "TContext.h"
---
+++ vs2015/TGON_Framework.System/TContext.h
@@ -0,0 +1,50 @@
+/*
+* Author : Junho-Cha
+* Date : 05/18/2015
+* Latest author : 
+* Latest date : 
+*/
+
+
+#pragma once
+#include <System/ISystem.h>
+#include <Core/TObject.h>
+#include <Config/BuildOption.h>
+
+namespace tgon
+{
+
+
+class TGON_API TContext : public TObject
+{
+public:
+	TGON_OBJECT( TContext, TObject )
+
+	// Register system : Time, ThreadPool, Graphics, Window, ... 
+	void RegisterSystem( ISystem* system );
+	
+	// Get system; Found ISystem pointer will be casted as T*
+	template<typename T> T* GetSystem( );
+
+	
+public:
+	TContext( ) {};
+	virtual ~TContext( ) {};
+
+private:
+	std::map<uintptr_t, ISystem*> m_systemRepo;
+};
+
+inline void tgon::TContext::RegisterSystem( ISystem* system )
+{
+	m_systemRepo[system->GetHashCode()] = system;
+}
+
+template<typename T>
+inline T* TContext::GetSystem( )
+{
+	return static_cast<T*>( m_systemRepo[system->GetHashCode()] );
+}
+
+
+}
