50ea06994cc52e36df224bbb066da9ee8720d346
ggomdyu <ggomdyu@gmail.com>
2016-08-09 16:01:05 +0900

    Update unique_comptr.h

--- Source/TGON/Platform/Utility/Windows/unique_comptr.h
+++ Source/TGON/Platform/Utility/Windows/unique_comptr.h
@@ -17,49 +17,87 @@ template <typename RawPtrTy>
 class unique_comptr final
 {
 public:
-	/*
-		Cons/Destructor
-	*/
+/*
+	Cons/Destructor
+*/
 	unique_comptr( );
 
 	~unique_comptr( );
 	
 	unique_comptr( RawPtrTy* rhs );
 
+	//
+	// Copy is disabled on unique_ptr because the pointer is subordinated to this
+	//
 	unique_comptr( const unique_comptr& rhs ) = delete;
 
+	//
+	// Change the pointer owner of right handling side.
+	//
+	// @param rhs original pointer owner
+	//
 	unique_comptr( unique_comptr&& rhs );
 
 
-	/*
-		Operators
-	*/
+/*
+	Operators
+*/
+	//
+	// Copy is disabled on unique_ptr because the pointer is subordinated to this
+	//
 	unique_comptr& operator=( const unique_comptr& rhs ) = delete;
 	
+	//
+	// Change the pointer owner of right handling side.
+	//
+	// @param rhs original pointer owner
+	//
 	unique_comptr& operator=( unique_comptr&& rhs );
 
+	//
+	// Compare each owner's raw pointer.
+	//
+	// @param rhs raw pointer owner
+	//
 	bool operator==( const unique_comptr& rhs );
 	
 	bool operator!=( const unique_comptr& rhs );
 	
+	//
+	// Return raw pointer's reference
+	//
 	RawPtrTy** operator&( );
 	
+	// Access like pointer 
 	RawPtrTy* operator->( ) const;
 
+	//
+	// Cast to raw pointer type.
+	//
 	operator RawPtrTy*( ) const;
 
 
 public:
-	/*
-		Commands
-	*/
+/*
+	Commands
+*/
+	//
 	// Release and attach the new pointer.
+	//
+	// @param rawPtr raw pointer you want to reset
+	//
 	void Reset( RawPtrTy* rawPtr );
 
+	//
 	// Relese the pointer.
+	//
 	void Release( );
 
+	//
 	// Get the real pointer.
+	//
+	// @return raw pointer
+	//
 	RawPtrTy* Get( );
 
 
