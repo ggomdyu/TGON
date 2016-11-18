d633cfda1cce54f17376d65d7d82228d4fb9117e
ggomdyu <ggomdyu@gmail.com>
2016-11-18 14:35:34 +0900

    Update WindowsApplication.h

--- Source/TGON/Core/Platform/Windows/WindowsApplication.h
+++ Source/TGON/Core/Platform/Windows/WindowsApplication.h
@@ -33,50 +33,52 @@ public:
 	WindowsApplication( ) = delete;
 	~WindowsApplication( ) = delete;
 
-	/*
+	/**
 	 * Commands
-	*/
+	 */
 public:
-	/*
-	 * @note	Update windows' message queue.
-	 * @return	False if there is no event to pull out
-	*/
+	/**
+	 * @note			Update windows' message queue.
+	 * @return			False if there is no event to pull out
+	 */
 	static bool PumpEvent( );
 	
-	/*
-	 * @note				Post quit message to its queue.
+	/**
+	 * @note			Post quit message to its queue.
 	 * @param	exitCode	Exit code
-	*/
+	 */
 	static void Quit( int32_t exitCode );
 
-	/*
-	 * @note				Terminate thread by force. Not recommended.
+	/**
+	 * @note			Terminate thread by force. Not recommended.
 	 * @param	exitCode	Exit code
-	*/
+	 */
 	static void ExitThread( int32_t exitCode );
 
-	/*
-	 * @note				Enable show cursor mode: Hide or Show
+	/**
+	 * @note			Enable show cursor mode: Hide or Show
 	 * @param	enableShow	Pass true to show, false otherwise.
-	*/
+	 */
 	static void ShowCursor( bool enableShow );
 
-	/* @note	Enable float calculate exception ( like devide 0, underflow, overflow, etc ) */
+	/**
+	 * @note			Enable float calculate exception like: devide by zero, underflow, overflow, etc 
+	 */
 	static void EnableFloatingException( unsigned int newStat );
 
 	/**/
 	//static void DisableTaskSwitching( );
 
-	/*
+	/**
 	 * Event handlers
-	*/
+	 */
 public:
 	/* @note	Message procedure */
 	static LRESULT WINAPI OnMessageHandled( HWND wndHandle, UINT msg, WPARAM wParam, LPARAM lParam );
 
-	/*
+	/**
 	 * Variables
-	*/
+	 */
 public:
 	static const HINSTANCE InstanceHandle;
 	static const wchar_t* AppClassName;
