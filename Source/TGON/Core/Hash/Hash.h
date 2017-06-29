b05090b3f191b7241d25436d3e4321d3db9a638f
ggomdyu <ggomdyu@gmail.com>
2017-06-30 01:58:39 +0900

    x65599 해싱 값을 컴파일 타임에 산출할 수 있게 함

--- Source/TGON/Core/Hash/Hash.h
+++ Source/TGON/Core/Hash/Hash.h
@@ -5,22 +5,34 @@
  */
 
 #pragma once
-#include "Core/Platform/Config.h"
-
 #include <cstddef>
+#include <boost/predef/compiler.h>
+
+#include "Core/Platform/Config.h"
 
 namespace tgon
 {
 namespace hash
 {
 
-using HashValue = std::size_t;
+#if (__cplusplus == 201402L)
+constexpr int32_t Hash::x65599Hash(const char* str)
+{
+    HashValue hashValue = 0;
+    for (std::size_t i = 0; str[i] != '\0'; ++i)
+    {
+        hashValue = 65599 * hashValue + str[i];
+    }
 
+    return hashValue ^ (hashValue >> 16);
+}
+#elif (__cplusplus == 201103L)
 // @brief   Generate x65599 hash by recursive.
-constexpr HashValue x65599Hash(const char* str, int prevHashValue = 0)
+constexpr int32_t x65599Hash(const char* str, int prevHashValue = 0)
 {
     return (str[0] != '\0') ? 65599 * x65599Hash(&str[1]) + str[0] : prevHashValue ^ (prevHashValue >> 16);
 }
+#endif
 
 } /* namespace hash */
 } /* namespace tgon */
