7f993dece3fe57460c202412b6f1f10aca608452
ggomdyu <ggomdyu@gmail.com>
2017-07-23 02:23:06 +0900

    멤버 함수를 위해 특수화된 traits의 ClassType 선언을 최소화 함

--- Source/TGON/Core/Utility/TypeTraits.h
+++ Source/TGON/Core/Utility/TypeTraits.h
@@ -16,80 +16,65 @@ namespace utility
 template <typename>
 struct FunctionTraits;
 
-/* @brief   Trait for lambda */
-template <typename Ty>
+/* @brief   Traits for lambda */
+template <typename _FunctionType>
 struct FunctionTraits : 
-	public FunctionTraits<decltype(&Ty::operator())>
-{
-public:
-    FunctionTraits() = delete;
-    ~FunctionTraits() = delete;
-};
-
-/* @brief   Trait for global function */
-template <typename _ReturnTy, typename... Args>
-struct FunctionTraits<_ReturnTy(Args...)>
-{
-public:
-    FunctionTraits() = delete;
-    ~FunctionTraits() = delete;
-
-public:
-	enum { ArgumentCount = sizeof...(Args), };
-
-	using ReturnTy = _ReturnTy;
-    using FunctionTy = _ReturnTy(Args...);
-    using FunctionPtrTy = _ReturnTy(*)(Args...);
-};
-
-template <typename ReturnTy, typename... Args>
-struct FunctionTraits<ReturnTy(*)(Args...)> :
-	public FunctionTraits<ReturnTy(Args...)>
+	public FunctionTraits<decltype(&_FunctionType::operator())>
 {
 };
 
-/* @brief   Trait for class member function */
-template <typename ReturnTy, typename _ClassTy, typename... Args>
-struct FunctionTraits<ReturnTy(_ClassTy::*)(Args...)> :
-	public FunctionTraits<ReturnTy(Args...)>
+/* @brief   Traits for global function */
+template <typename _ReturnType, typename... _Args>
+struct FunctionTraits<_ReturnType(_Args...)>
 {
-public:
-	using ClassTy = _ClassTy;
+	enum { ArgumentCount = sizeof...(_Args), };
+
+	using ReturnType = _ReturnType;
+
+    using FunctionType = _ReturnType(_Args...);
+    using FunctionPtrType = _ReturnType(*)(_Args...);
 };
 
-template <typename ReturnTy, typename _ClassTy, typename... Args>
-struct FunctionTraits<ReturnTy(_ClassTy::*)(Args...) const> :
-	public FunctionTraits<ReturnTy(Args...)>
+template <typename _ReturnType, typename... _Args>
+struct FunctionTraits<_ReturnType(*)(_Args...)> :
+	public FunctionTraits<_ReturnType(_Args...)>
 {
-public:
-	using ClassTy = _ClassTy;
 };
 
-template <typename ReturnTy, typename _ClassTy, typename... Args>
-struct FunctionTraits<ReturnTy(_ClassTy::*)(Args...) volatile> :
-	public FunctionTraits<ReturnTy(Args...)>
+/* @brief   Traits for class member function */
+template <typename _ReturnType, typename _ClassType, typename... _Args>
+struct FunctionTraits<_ReturnType(_ClassType::*)(_Args...)> :
+	public FunctionTraits<_ReturnType(_Args...)>
 {
-public:
-	using ClassTy = _ClassTy;
+	using ClassType = _ClassType;
 };
 
-template <typename ReturnTy, typename _ClassTy, typename... Args>
-struct FunctionTraits<ReturnTy(_ClassTy::*)(Args...) const volatile> :
-    public FunctionTraits<ReturnTy(Args...)>
+template <typename _ReturnType, typename _ClassType, typename... _Args>
+struct FunctionTraits<_ReturnType(_ClassType::*)(_Args...) const> :
+	public FunctionTraits<_ReturnType(_ClassType::*)(_Args...)>
 {
-public:
-    using ClassTy = _ClassTy;
 };
 
-/* @brief   Enable if */
-template <typename ConvertFromTy, typename ConvertToTy>
-using EnableIfConvertible = typename std::enable_if<std::is_convertible<ConvertFromTy, ConvertToTy>::value>::type;
+template <typename _ReturnType, typename _ClassType, typename... _Args>
+struct FunctionTraits<_ReturnType(_ClassType::*)(_Args...) volatile> :
+	public FunctionTraits<_ReturnType(_ClassType::*)(_Args...)>
+{
+};
 
-template <typename Ty>
-using EnableIfFloatingPoint = typename std::enable_if<std::is_floating_point<Ty>::value>::type;
+template <typename _ReturnType, typename _ClassType, typename... _Args>
+struct FunctionTraits<_ReturnType(_ClassType::*)(_Args...) const volatile> :
+    public FunctionTraits<_ReturnType(_ClassType::*)(_Args...)>
+{
+};
 
-template <typename Ty>
-using EnableIfArithmetic = typename std::enable_if<std::is_arithmetic<Ty>::value>::type;
+template <typename _ConvertFromType, typename _ConvertToType>
+using EnableIfConvertible = typename std::enable_if<std::is_convertible<_ConvertFromType, _ConvertToType>::value>::type;
+
+template <typename _ValueType>
+using EnableIfFloatingPoint = typename std::enable_if<std::is_floating_point<_ValueType>::value>::type;
+
+template <typename _ValueType>
+using EnableIfArithmetic = typename std::enable_if<std::is_arithmetic<_ValueType>::value>::type;
 
 } /* namespace utility */
 } /* namespace tgon */
