a79eb7b054c9b9056e2a8cfa720801bd94f5bdaa
ggomdyu <ggomdyu@gmail.com>
2017-08-17 01:40:51 +0900

    MacOSTime 추가

---
+++ Source/TGON/Core/Platform/macOS/MacOSTime.mm
@@ -0,0 +1,40 @@
+#include "PrecompiledHeader.pch"
+#include "MacOSTime.h"
+
+#include <mach/mach_time.h>
+#include <ctime>
+
+namespace tgon
+{
+namespace platform
+{
+
+TGON_API uint64_t GetOSExecutionTime()
+{
+    // todo: Need test
+    return mach_absolute_time() * 0.000001f;
+}
+
+TGON_API DateTime GetSystemTime()
+{
+    std::time_t unixTime = std::time(nullptr);
+
+    std::tm rawUtcTime;
+    gmtime_r(&unixTime, &rawUtcTime);
+
+    return DateTime(rawUtcTime);
+}
+
+TGON_API DateTime GetLocalTime()
+{
+    std::time_t unixTime = std::time(nullptr);
+
+    std::tm rawLocalTime;
+    gmtime_r(&unixTime, &rawLocalTime);
+    localtime_r(&unixTime, &rawLocalTime);
+
+    return DateTime(rawLocalTime);
+}
+
+} /* namespace platform */
+} /* namespace tgon */
