e299483980f4475ab4f88168c98a2dd7ad0726c1
ggomdyu <ggomdyu@gmail.com>
2017-08-17 01:40:13 +0900

    MacOSTime 추가

---
+++ Source/TGON/Core/Platform/MacO/MacOSTime.h
@@ -0,0 +1,20 @@
+/**
+ * @filename    MacOSTime.h
+ * @author      ggomdyu
+ * @since       08/15/2017
+ */
+
+#pragma once
+#include "../Base/BaseTime.h"
+
+namespace tgon
+{
+namespace platform
+{
+namespace macos
+{
+
+
+} /* namespace windows */
+} /* namespace platform */
+} /* namespace tgon */
