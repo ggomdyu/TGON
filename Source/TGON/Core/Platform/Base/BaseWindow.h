0faac90ecc4d73e21609af39d855661a3bcd9abf
ggomdyu <ggomdyu@gmail.com>
2017-09-05 22:43:55 +0900

    CRTP를 이용한 정적 추상화 -> 동적 추상화로 바

--- Source/TGON/Core/Platform/Base/BaseWindow.h
+++ Source/TGON/Core/Platform/Base/BaseWindow.h
@@ -2,7 +2,7 @@
  * @filename    BaseWindow.h
  * @author      ggomdyu
  * @since       01/09/2016
- * @desc        Set of Platform-agnostic Window interface.
+ * @brief       Set of Platform-agnostic Window interface.
  */
 
 #pragma once
@@ -10,148 +10,61 @@
 #include <cstdint>
 #include <boost/noncopyable.hpp>
 
-#include "BaseWindowType.h"
-
 #include "Core/Platform/Config.h"
+
+#include "BaseWindowType.h"
 
 namespace tgon
 {
 namespace platform
 {
 
-template <typename _DerivedType>
 class TGON_API BaseWindow
 {
-/* @section Ctor/Dtor */
+/* @section Public constructor */
 public:
     BaseWindow();
-    ~BaseWindow();
-    
+    BaseWindow(const BaseWindow&) = delete;
+
+/* @section Protected destructor */
+public:
+    virtual ~BaseWindow();
+
+/* @section Public operator */
+public:
+    BaseWindow& operator=(const BaseWindow&) = delete;
+
 /* @section Public method */
 public:
-    void Quit();
-    void Maximize();
-    void Minimize();
-    void BringToFront();
+    virtual void Quit() {}
+    virtual void Maximize() {}
+    virtual void Minimize() {}
+    virtual void BringToFront() {}
 
-    void SetPosition(int32_t x, int32_t y);
-    void SetSize(int32_t width, int32_t height);
-    void SetCaptionTitle(const char* captionTitle);
-    void GetPosition(int32_t* x, int32_t* y) const;
-    void GetSize(int32_t* width, int32_t* height) const;
-    void GetCaptionTitle(char* destCaptionTitle) const;
-    bool HasCaption() const;
-    bool IsResizable() const;
-    bool IsMaximized() const;
-    bool IsMinimized() const;
-    bool IsClosed() const noexcept;
+    virtual void SetPosition(int32_t x, int32_t y) {}
+    virtual void SetSize(int32_t width, int32_t height) {}
+    virtual void SetTitle(const char* title) {}
+    virtual void SetFullScreen(bool isFullScreen) {}
+    virtual void GetPosition(int32_t* x, int32_t* y) const {}
+    virtual void GetSize(int32_t* width, int32_t* height) const {}
+    virtual void GetCaptionTitle(char* destCaptionTitle) const {}
+    virtual bool HasCaption() const;
+    virtual bool IsResizable() const;
+    virtual bool IsMaximized() const;
+    virtual bool IsMinimized() const;
+    virtual bool IsClosed() const noexcept;
+
+    virtual void OnMove(int32_t x, int32_t y) {}
+    virtual void OnResize(int32_t width, int32_t height) {}
+    virtual void OnMaximize() {}
+    virtual void OnMinimize() {}
+    virtual void OnEnterFullScreen() {}
+    virtual void OnExitFullScreen() {}
 
 /* @section Protected variable */
-protected:
+private:
     bool m_isClosed;
 };
-
-template <typename _DerivedType>
-inline BaseWindow<_DerivedType>::BaseWindow() :
-    m_isClosed(false)
-{
-}
-
-template <typename _DerivedType>
-inline BaseWindow<_DerivedType>::~BaseWindow() 
-{
-    m_isClosed = true;
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::Quit()
-{
-    static_cast<_DerivedType*>(this)->Quit();
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::Maximize()
-{
-    static_cast<_DerivedType*>(this)->Maximize();
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::Minimize()
-{
-    static_cast<_DerivedType*>(this)->Minimize();
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::BringToFront()
-{
-    static_cast<_DerivedType*>(this)->BringToFront();
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::SetPosition(int32_t x, int32_t y)
-{
-    static_cast<_DerivedType*>(this)->SetPosition(x, y);
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::SetSize(int32_t width, int32_t height)
-{
-    static_cast<_DerivedType*>(this)->SetSize(width, height);
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::SetCaptionTitle(const char* captionTitle)
-{
-    static_cast<_DerivedType*>(this)->SetCaptionTitle(captionTitle);
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::GetPosition(int32_t* x, int32_t* y) const
-{
-    static_cast<_DerivedType*>(this)->GetPosition(x, y);
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::GetSize(int32_t* width, int32_t* height) const
-{
-    static_cast<_DerivedType*>(this)->GetSize(width, height);
-}
-
-template <typename _DerivedType>
-inline void BaseWindow<_DerivedType>::GetCaptionTitle(char* destCaptionTitle) const
-{
-    static_cast<_DerivedType*>(this)->GetCaptionTitle(destCaptionTitle);
-}
-
-template <typename _DerivedType>
-inline bool BaseWindow<_DerivedType>::HasCaption() const
-{
-    return static_cast<_DerivedType*>(this)->HasCaption();
-}
-
-template <typename _DerivedType>
-inline bool BaseWindow<_DerivedType>::IsResizable() const
-{
-    return static_cast<_DerivedType*>(this)->IsResizable();
-}
-
-template <typename _DerivedType>
-inline bool BaseWindow<_DerivedType>::IsMaximized() const
-    {
-        return static_cast<_DerivedType*>(this)->IsMaximized();
-}
-
-template <typename _DerivedType>
-inline bool BaseWindow<_DerivedType>::IsMinimized() const
-{
-    return static_cast<_DerivedType*>(this)->IsMinimized();
-}
-
-template <typename _DerivedType>
-inline bool BaseWindow<_DerivedType>::IsClosed() const noexcept
-{
-	return static_cast<_DerivedType*>(this)->IsClosed();
-}
 
 } /* namespace platform */
 } /* namespace tgon */
