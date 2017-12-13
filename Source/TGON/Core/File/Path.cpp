7abb374515375a1850a97a540eb80ee13753913f
ggomdyu <ggomdyu@gmail.com>
2017-12-14 00:00:52 +0900

    Path.h 부활

---
+++ Source/TGON/Core/File/Path.cpp
@@ -0,0 +1,81 @@
+#include "PrecompiledHeader.pch"
+#include "Path.h"
+
+namespace tgon
+{
+namespace file
+{
+
+TGON_API std::size_t GetExtension(const char* srcStr, char* destStr)
+{
+    return 0;
+}
+
+TGON_API std::string GetExtension(const char* srcStr)
+{
+    return std::string();
+}
+
+TGON_API std::size_t GetFileName(const char* srcStr, char* destStr)
+{
+    auto length = std::strlen(srcStr);
+
+    auto index = length - 1;
+    if (index <= 0)
+    {
+        return 0;
+    }
+
+    while (srcStr[index] != '/' || srcStr[index] != '\\')
+    {
+        if (index-- <= 0)
+        {
+            return 0;
+        }
+    }
+
+    return length - (index + 1);
+}
+
+TGON_API std::string GetFileName(const char* srcStr)
+{
+    auto length = std::strlen(srcStr);
+
+    int32_t index = length - 1;
+    if (index <= 0)
+    {
+        return nullptr;
+    }
+
+    while (srcStr[index] != '/' || srcStr[index] != '\\')
+    {
+        if (index <= 0)
+        {
+            return nullptr;
+        }
+
+        --index;
+    }
+
+    return std::string(&srcStr[index]);
+}
+
+TGON_API std::size_t GetFileNameWithoutExtension(const char* srcStr, char* destStr)
+{
+    //if (deleteFileExtension)
+    //{
+    //    // delete file extension
+    //    while ((*destStr)[index--] != '.')
+    //    {
+    //        destStr->pop_back();
+    //    }
+    //
+    //    // delete '.'
+    //    destStr->pop_back();
+    //}
+
+    return 0;
+}
+
+} /* namespace file */
+} /* namespace tgon */
---
+++ Source/TGON/Core/File/Path.h
@@ -0,0 +1,82 @@
+/**
+ * @filename    Path.h
+ * @author      ggomdyu
+ * @since       04/07/2017
+ */
+
+#pragma once
+#include <cstdint>
+#include <string>
+
+#include "Core/Platform/Config.h"
+
+namespace tgon
+{
+namespace file
+{
+
+/**
+ * @param [in]  srcStr  
+ * @param [out] destStr 
+ * @return  
+ */
+TGON_API std::size_t GetExtension(const char* srcStr, char* destStr);
+
+/**
+ * @param [in]  srcStr  
+ * @return  
+ */
+TGON_API std::string GetExtension(const char* srcStr);
+
+/**
+ * @param [in]  srcStr  
+ * @param [out] destStr 
+ * @return  
+ */
+TGON_API std::size_t GetFileName(const char* srcStr, char* destStr);
+
+/**
+ * @param [in]  srcStr  
+ * @return  
+ */
+TGON_API std::string GetFileName(const char* srcStr);
+
+/**
+ * @param [in]  srcStr  
+ * @param [out] destStr 
+ * @return  
+ */
+TGON_API std::size_t GetFileNameWithoutExtension(const char* srcStr, char* destStr);
+
+/**
+ * @param [in]  srcStr  
+ * @return  
+ */
+TGON_API std::string GetFileNameWithoutExtension(const char* srcStr);
+
+//public static readonly char AltDirectorySeparatorChar;
+//public static readonly char DirectorySeparatorChar;
+//[Obsolete("see GetInvalidPathChars and GetInvalidFileNameChars methods.")]
+//public static readonly char[] InvalidPathChars;
+//public static readonly char PathSeparator;
+//public static readonly char VolumeSeparatorChar;
+//
+//public static string ChangeExtension(string path, string extension);
+//public static string Combine(params string[] paths);
+//public static string Combine(string path1, string path2);
+//public static string Combine(string path1, string path2, string path3);
+//public static string Combine(string path1, string path2, string path3, string path4);
+//public static string GetDirectoryName(string path);
+//public static string GetFullPath(string path);
+//public static char[] GetInvalidFileNameChars();
+//public static char[] GetInvalidPathChars();
+//public static string GetPathRoot(string path);
+//public static string GetRandomFileName();
+//public static string GetTempFileName();
+//public static string GetTempPath();
+//public static bool HasExtension(string path);
+//public static bool IsPathRooted(string path);
+
+};
+
+} /* namespace tgon */
