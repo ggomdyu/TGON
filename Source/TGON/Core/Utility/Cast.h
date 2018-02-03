635bb10c2f97fb17ed169fea34aae515d31e5fb5
ggomdyu <ggomdyu@gmail.com>
2018-02-03 14:27:32 +0900

    코드 정리

---
+++ Source/TGON/Core/Utility/Cast.h
@@ -0,0 +1,83 @@
+/**
+ * @filename    Cast.h
+ * @author      ggomdyu
+ * @since       05/24/2016
+ */
+
+#pragma once
+#include <utility>
+
+namespace tgon
+{
+namespace core
+{
+namespace detail
+{
+
+/**
+ * @class   AutoCaster
+ * @brief   Automatic type casting helper
+ */
+template <typename _CastFromType, typename _CastPolicyType>
+struct AutoCaster final :
+    public _CastPolicyType
+{
+/* @section Public constructor */
+public:
+    constexpr explicit AutoCaster(_CastFromType&& castFromPtr) noexcept :
+        m_castFromPtr(castFromPtr)
+    {
+    }
+
+/* @section Public operator */
+public:
+    template <typename _CastToType>
+    constexpr operator _CastToType() const noexcept
+    {
+        return _CastPolicyType::template Cast<_CastFromType, _CastToType>(m_castFromPtr);
+    }
+
+/* @section Private variable */
+private:
+    _CastFromType m_castFromPtr;
+};
+
+
+struct SafeCastPolicy
+{
+/* @section Protected method */
+protected:
+    template <typename _CastFromType, typename _CastToType>
+    constexpr _CastToType Cast(_CastFromType&& castFromPtr) const noexcept
+    {
+        return static_cast<_CastToType>(std::forward<_CastFromType>(castFromPtr));
+    }
+};
+
+struct ForceCastPolicy
+{
+/* @section Protected method */
+protected:
+    template <typename _CastFromType, typename _CastToType>
+    constexpr _CastToType Cast(_CastFromType&& castFromPtr) const noexcept
+    {
+        return reinterpret_cast<_CastToType>(std::forward<_CastFromType>(castFromPtr));
+    }
+};
+
+} /* namespace detail */
+
+template <typename _CastFromType>
+constexpr detail::AutoCaster<_CastFromType, detail::SafeCastPolicy> AutoCast(_CastFromType&& rhs) noexcept
+{
+	return detail::AutoCaster<_CastFromType, detail::SafeCastPolicy>(std::forward<_CastFromType>(rhs));
+}
+
+template <typename _CastFromType>
+constexpr detail::AutoCaster<_CastFromType, detail::ForceCastPolicy> ForceAutoCast(_CastFromType&& rhs) noexcept
+{
+	return detail::AutoCaster<_CastFromType, detail::ForceCastPolicy>(std::forward<_CastFromType>(rhs));
+}
+
+} /* namespace core */
+} /* namespace tgon */
