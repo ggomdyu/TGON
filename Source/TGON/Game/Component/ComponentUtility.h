d32ad1fa6df8f650b4b3feec7aa5359f3f4aa7e3
ggomdyu <ggomdyu@gmail.com>
2018-06-18 22:51:30 +0900

    Header added

---
+++ Source/TGON/Game/Component/ComponentUtility.h
@@ -0,0 +1,24 @@
+/**
+ * @file    ComponentUtility.h
+ * @author  ggomdyu
+ * @since   06/18/2018
+ */
+
+#pragma once
+#include "Core/Object/IRuntimeObjectUtility.h"
+
+namespace tgon
+{
+
+/**
+ * @brief                   Gets the unique id of component specified by _ComponentType.
+ * @tparam _ComponentType   The type of Component what you want to get id.
+ * @return                  The unique id of component.
+ */
+template <typename _ComponentType>
+inline std::size_t GetComponentId()
+{
+    return tgon::GetRTTI<_ComponentType>()->GetHashCode();
+}
+
+} /* namespace tgon */
