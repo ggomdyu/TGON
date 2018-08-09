b26553b52c3c8afa241ee685962e673e51fc2eb4
ggomdyu <ggomdyu@gmail.com>
2018-08-10 00:14:38 +0900

    - 코드 정리 - 변수에 대해서는 자동 AutoCast가 안되는 문제 수정

--- Source/TGON/Core/Utility/AutoCast.h
+++ Source/TGON/Core/Utility/AutoCast.h
@@ -9,71 +9,84 @@
 
 namespace tgon
 {
-namespace detail
-{
 
-/**
- * @class   AutoCastProxy
- * @brief   Automatic type casting helper
- */
 template <typename _CastFromType, typename _CastPolicyType>
-struct AutoCastProxy final :
-    public _CastPolicyType
+struct AutoCaster final :
+    private _CastPolicyType
 {
 /* @section Public constructor */
 public:
-    constexpr explicit AutoCastProxy(_CastFromType&& castFromPtr) noexcept :
-        m_castFromPtr(castFromPtr)
-    {
-    }
+    template <typename _CastFromType2>
+    constexpr explicit AutoCaster(_CastFromType2&& castFromValue) noexcept;
 
 /* @section Public operator */
 public:
     template <typename _CastToType>
-    constexpr operator _CastToType() const noexcept
-    {
-        return _CastPolicyType::template Cast<_CastFromType, _CastToType>(m_castFromPtr);
-    }
+    constexpr operator _CastToType() const noexcept;
 
 /* @section Private variable */
 private:
-    _CastFromType m_castFromPtr;
+    _CastFromType m_castFromValue;
 };
+
+template <typename _CastFromType, typename _CastPolicyType>
+template <typename _CastFromType2>
+constexpr AutoCaster<_CastFromType, _CastPolicyType>::AutoCaster(_CastFromType2&& castFromValue) noexcept :
+    m_castFromValue(std::forward<_CastFromType2>(castFromValue))
+{
+}
+
+template <typename _CastFromType, typename _CastPolicyType>
+template <typename _CastToType>
+constexpr AutoCaster<_CastFromType, _CastPolicyType>::operator _CastToType() const noexcept
+{
+    return _CastPolicyType::template Cast<_CastFromType, _CastToType>(m_castFromValue);
+}
 
 struct SafeCastPolicy
 {
 /* @section Protected method */
 protected:
     template <typename _CastFromType, typename _CastToType>
-    constexpr _CastToType Cast(_CastFromType&& castFromPtr) const noexcept
-    {
-        return static_cast<_CastToType>(std::forward<_CastFromType>(castFromPtr));
-    }
+    constexpr _CastToType Cast(const _CastFromType& castFromValue) const noexcept;
 };
+
+template <typename _CastFromType, typename _CastToType>
+constexpr _CastToType SafeCastPolicy::Cast(const _CastFromType& castFromValue) const noexcept
+{
+    return static_cast<_CastToType>(castFromValue);
+}
 
 struct ForceCastPolicy
 {
 /* @section Protected method */
 protected:
     template <typename _CastFromType, typename _CastToType>
-    constexpr _CastToType Cast(_CastFromType&& castFromPtr) const noexcept
-    {
-        return reinterpret_cast<_CastToType>(std::forward<_CastFromType>(castFromPtr));
-    }
+    constexpr _CastToType Cast(const _CastFromType& castFromValue) const noexcept;
 };
 
-} /* namespace detail */
-
-template <typename _CastFromType>
-constexpr detail::AutoCastProxy<_CastFromType, detail::SafeCastPolicy> AutoCast(_CastFromType&& rhs) noexcept
+template <typename _CastFromType, typename _CastToType>
+constexpr _CastToType ForceCastPolicy::Cast(const _CastFromType& castFromValue) const noexcept
 {
-	return detail::AutoCastProxy<_CastFromType, detail::SafeCastPolicy>(std::forward<_CastFromType>(rhs));
+    return reinterpret_cast<_CastToType>(castFromValue);
 }
 
 template <typename _CastFromType>
-constexpr detail::AutoCastProxy<_CastFromType, detail::ForceCastPolicy> ForceAutoCast(_CastFromType&& rhs) noexcept
+using SafeAutoCaster = AutoCaster<_CastFromType, SafeCastPolicy>;
+
+template <typename _CastFromType>
+using ForceAutoCaster = AutoCaster<_CastFromType, ForceCastPolicy>;
+
+template <typename _CastFromType>
+constexpr SafeAutoCaster<_CastFromType> AutoCast(_CastFromType&& value) noexcept
 {
-	return detail::AutoCastProxy<_CastFromType, detail::ForceCastPolicy>(std::forward<_CastFromType>(rhs));
+	return SafeAutoCaster<_CastFromType>(std::forward<_CastFromType>(value));
+}
+
+template <typename _CastFromType>
+constexpr ForceAutoCaster<_CastFromType> ForceAutoCast(_CastFromType&& value) noexcept
+{
+	return ForceAutoCaster<_CastFromType>(std::forward<_CastFromType>(value));
 }
 
 } /* namespace tgon */
