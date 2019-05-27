5dc2a6c6e5d1eb864ef951963844be2649abd1e7
ggomdyu <ggomdyu@gmail.com>
2019-05-27 16:25:15 +0900

    Update FixedString.h

--- Source/TGON/String/FixedString.h
+++ Source/TGON/String/FixedString.h
@@ -136,7 +136,7 @@ using FixedWString1024 = BasicFixedString<wchar_t, 1024>;
 template <typename _CharType, std::size_t _CharArraySize, typename _StringTraitsType = BasicStringTraits<_CharType>>
 constexpr auto MakeFixedString(const _CharType(&str)[_CharArraySize]) -> BasicFixedString<_CharType, _CharArraySize, _StringTraitsType>
 {
-    return MakeFixedString(str, _CharArraySize - 1);
+    return {str, _CharArraySize - 1};
 }
 
 template <typename _CharType, std::size_t _CharArraySize, typename _StringTraitsType>
