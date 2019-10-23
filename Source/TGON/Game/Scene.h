8d449312aa9465c1bcb1809c4418191f6c18e0de
ggomdyu <ggomdyu@gmail.com>
2019-10-23 17:55:15 +0900

    Update Scene.h

--- Source/TGON/Game/Scene.h
+++ Source/TGON/Game/Scene.h
@@ -30,20 +30,19 @@ public:
     virtual void Update();
     void AddObject(const std::shared_ptr<GameObject>& object);
     void AddObject(std::shared_ptr<GameObject>&& object);
-    void AddGlobalObject(const std::shared_ptr<GameObject>& object);
     bool RemoveObject(const StringViewHash& objectName);
-    bool RemoveGlobalObject(const StringViewHash& objectName);
     std::shared_ptr<GameObject> FindObject(const StringViewHash& objectName);
     std::shared_ptr<const GameObject> FindObject(const StringViewHash& objectName) const;
-    std::shared_ptr<GameObject> FindGlobalObject(const StringViewHash& objectName);
-    std::shared_ptr<const GameObject> FindGlobalObject(const StringViewHash& objectName) const;
+    static void AddGlobalObject(const std::shared_ptr<GameObject>& object);
+    static bool RemoveGlobalObject(const StringViewHash& objectName);
+    static std::shared_ptr<GameObject> FindGlobalObject(const StringViewHash& objectName);
+    static std::shared_ptr<const GameObject> FindGlobalObject(const StringViewHash& objectName) const;
 
 /**@section Variable */
 private:
     std::vector<std::shared_ptr<GameObject>> m_objects;
-    static std::vector<std::shared_ptr<GameObject>> m_globalObjects;
-    
     std::unordered_map<size_t, int32_t> m_objectNameHashMap;
+    static std::vector<std::shared_ptr<GameObject>> m_globalObjects;
     static std::unordered_map<size_t, int32_t> m_globalObjectNameHashMap;
 };
 
