5782b00bc1f4e588c0b23c39185b9bbf4f35a52f
ggomdyu <ggomdyu@gmail.com>
2020-01-08 02:25:15 +0900

    커밋

--- Source/Audio/OpenALDebug.h
+++ Source/Audio/OpenALDebug.h
@@ -5,7 +5,6 @@
  */
 
 #pragma once
-#if TGON_GRAPHICS_OPENGL
 #include <fmt/format.h>
 
 #include "Diagnostics/Debug.h"
@@ -22,5 +21,4 @@
     }
 #else
 #   define TGON_GL_ERROR_CHECK(expression) expression
-#endif
 #endif
