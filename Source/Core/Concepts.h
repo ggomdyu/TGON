fe7abde64635ce87681cf15ce0e7d5046137c9b9
ggomdyu <ggomdyu@gmail.com>
2020-07-17 23:09:00 +0900

    Add Indexable, Cloneable

--- Source/Core/Concepts.h
+++ Source/Core/Concepts.h
@@ -10,8 +10,15 @@ template <typename _Type>
 concept Arithmetic = std::is_arithmetic_v<_Type>;
 
 template <typename _Type>
-concept Hashable = requires {
-    std::hash<_Type>{}();
+concept Hashable = requires(_Type value)
+{
+    { std::hash<_Type>{}(value) } -> std::convertible_to<size_t>;
 };
+
+template <typename _Type>
+concept Indexable = requires(_Type value) { value[0]; };
+
+template <typename _Type>
+concept Cloneable = requires(_Type* value) { value->Clone(); };
 
 }
